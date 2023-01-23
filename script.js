fe0a315ca9f823e9ddd91a67bd90d66f334e1497
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-01-23 11:38:52 +0100

    map done

--- script.js
+++ script.js
@@ -16,8 +16,50 @@ let algo = randomNumbers.forEach( (element, index) => {
     console.log("la respuesta a la vida y a todo")
   }
 
+  // el forEach nunca espera un return 
+  // no hace falta return en forEach
+
 } )
 
 console.log(algo) // undefined
 // no retorna nada, solo hace una ejecucion sobre cada elemento del array
 
+
+
+// .map()
+
+let algoMap = randomNumbers.map( (element, index) => {
+
+  console.log(".map por cada elemento", element, index)
+
+  // siempre requiere un return
+  // crear un nuevo array con los valores retornados
+
+  // if (element > 10) {
+    return `El valor es: ${element * 100}`
+  // }
+
+} )
+
+console.log(algoMap)
+
+// el nuevo array SIEMPRE va a tener la misma cantidad de elementos que el arr original
+
+
+// creamos una funcion que reciba un array de numeros y cree un nuevo array donde los valores sean string
+
+function numbersAsString(arrNum) {
+
+  // crea nuevo array de strings
+  let newArr = arrNum.map( (element, index) => {
+    // retornar lo que será cada elemento del nuevo arr
+    return element.toString()
+  } )
+
+  // console.log(newArr)
+  return newArr
+
+}
+
+console.log( numbersAsString(randomNumbers) )
+console.log( numbersAsString([1, 2, 3]) )
